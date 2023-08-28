fc01e2b15d399b2ece0927d514521ed7e7c50bb6
minhazroot <minhazuddin180016@gmail.com>
2023-08-29 00:22:25 +0600

    complit this project

--- js/phone.js
+++ js/phone.js
@@ -1,13 +1,13 @@
-const LodePhone = async (SearchText) => {
+const LodePhone = async (SearchText = 13, IsShowAll) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${SearchText}`)
     const data = await res.json();
     const phones = data.data;
     // console.log(phones)
-    DisplayPhone(phones)
+    DisplayPhone(phones, IsShowAll)
 }
 
-const DisplayPhone = phones => {
-    // console.log(phones)
+const DisplayPhone = (phones, IsShowAll) => {
+
     // step 1 Get contener wher to set 
     const PhoneContener = document.getElementById('Pone_contener')
 
@@ -15,14 +15,17 @@ const DisplayPhone = phones => {
     PhoneContener.innerHTML = ''
     //display show all button before 12 phone 
     const ShowAll = document.getElementById('show_all_Conty')
-    if (phones.length > 20) {
+    if (phones.length > 20 && !IsShowAll) {
 
         ShowAll.classList.remove('hidden')
     } else {
         ShowAll.classList.add('hidden')
     }
-    //display lemit itea  10
-    phones = phones.slice(0, 9)
+
+    //display lemit itea  10 if not show all 
+    if (!IsShowAll) {
+        phones = phones.slice(0, 9)
+    }
     phones.forEach(phone => {
         // console.log(phone)
         //step 2:creat a div 
@@ -36,8 +39,8 @@ const DisplayPhone = phones => {
                     <div class="card-body">
                         <h2 class="card-title">${phone.phone_name}</h2>
                         <p>${phone.slug}</p>
-                        <div class="card-actions justify-end">
-                            <button class="btn btn-primary">Buy Now</button>
+                        <div class="card-actions justify-center">
+                            <button onclick="HandelShowDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
                         </div>
                     </div>
         
@@ -57,12 +60,12 @@ const DisplayPhone = phones => {
 }
 //hendel search  
 
-const handleSearch = () => {
+const handleSearch = (IsShowAll) => {
     // loading-spinner desplay
     toggleLoadingSpinner(true)
     const SearchFiled = document.getElementById('Searce_Input')
     const SearchText = SearchFiled.value;
-    LodePhone(SearchText)
+    LodePhone(SearchText, IsShowAll)
 }
 
 // loading-spinner function 
@@ -73,6 +76,43 @@ const toggleLoadingSpinner = (IsLoading) => {
     } else {
         LoadingSpinner.classList.add('hidden')
     }
+}
+//handel Show All button
+const ShowAll = () => {
+    handleSearch(true)
+}
+// handel Show Details
+const HandelShowDetails = async (ProductId) => {
+    // console.log('show details', ProductId);
+    // lode single phone data 
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${ProductId}`)
+    const Data = await res.json()
+    const Details = Data.data
+
+
+    PhoneDetails(Details)
+}
+// show phone Detals 
+const PhoneDetails = (PhoneInformation) => {
+    console.log(PhoneInformation)
+
+    Show_Details_modal.showModal()
+
+    //display details
+    const DetailsContainer = document.getElementById('Detels_container')
+    DetailsContainer.innerHTML = `   
+    <img src="${PhoneInformation.image}" alt="">     
+    <h3 class="font-bold text-lg"><span>Name:-</span> ${PhoneInformation.name}</h3>
+    <p><span>brand:-</span> ${PhoneInformation.brand}</p>
+    <p><span>Storage:-</span> ${PhoneInformation?.mainFeatures?.memory}</p>
+    <p><span>chipSet:-</span> ${PhoneInformation?.mainFeatures?.chipSet}</p>
+    <p><span>Display:-</span> ${PhoneInformation?.mainFeatures?.displaySize}</p>
+    <p><span>ReleaseDate:-</span> ${PhoneInformation?.releaseDate}</p>
+    <p><span>slug :-</span> ${PhoneInformation?.slug}</p>
+        
+    
+    
+    `
 }
 
 LodePhone()
